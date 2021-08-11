ebe5b5e8cd1e7c36daeff4bbd415c8494834b14a
cboscariol <caamila.boscariol@gmail.com>
2021-08-11 11:55:09 -0300

    persiste dados stepper

--- src/Components/CadastroPrimeiroPasso/index.js
+++ src/Components/CadastroPrimeiroPasso/index.js
@@ -1,23 +1,49 @@
+import { useEffect } from 'react';
 import useStyles from './styles';
 import './styles.css'
 import {
 	TextField,
 	Button,
 	Typography,
-
 } from '@material-ui/core';
 import InputSenha from '../InputSenha/inputSenha'
 import { useForm } from "react-hook-form";
 
 
-function CadastroPrimeiroPasso({ nextPage, setPayload }) {
+function CadastroPrimeiroPasso({ nextPage, setPayload, payload }) {
 	const classes = useStyles();
-	const { register, getValues, handleSubmit, watch, formState: { errors } } = useForm();
+	const { register, getValues, handleSubmit, watch, setValue, formState: { errors } } = useForm();
 
 	const onSubmit = () => {
 		setPayload((currentPayload) => ({ ...currentPayload, ...getValues() }))
 		nextPage();
+
 	}
+
+	// setValue("nome", payload.nome)
+
+	// setValue("email", result.usuario.email)
+	// setValue("restaurante_nome", result.restaurante.nome)
+	// setValue("descricao", result.restaurante.descricao)
+	// setValue("categoria_id", result.restaurante.categoria_id)
+	// setValue("taxa_entrega", currencyFormatter(result.restaurante.taxa_entrega))
+	// setValue("tempo_entrega_minutos", result.restaurante.tempo_entrega_minutos)
+	// setValue("valor_minimo_pedido", currencyFormatter(result.restaurante.valor_minimo_pedido))
+
+	const getPayload = () => {
+		payload.nome && setValue("nome", payload.nome)
+		payload.email && setValue("email", payload.email)
+	}
+
+	useEffect(() => {
+		getPayload()
+	}, [])
+
+
+
+
+
+
 
 	return (
 		<div className={classes.root}>
@@ -28,6 +54,7 @@ function CadastroPrimeiroPasso({ nextPage, setPayload }) {
 						id="input-nome"
 						type='text'
 						variant="outlined"
+						defaultValue={payload.nome}
 						autoComplete="off"
 						error={Boolean(errors.nome)}
 						helperText={errors.nome ? "Campo Obrigatório" : false}
@@ -38,6 +65,7 @@ function CadastroPrimeiroPasso({ nextPage, setPayload }) {
 						id="input-email"
 						type='email'
 						variant="outlined"
+						defaultValue={payload.email}
 						autoComplete="off"
 						error={Boolean(errors.email)}
 						helperText={errors.email ? "Campo Obrigatório" : false}
